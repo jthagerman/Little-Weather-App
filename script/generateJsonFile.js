b9d843823d26f022a99579d13d5a9e10eb9556e1
John Hagerman <jthagerma@gmail.com>
2021-08-12 16:05:28 -0400

    settup db and locatrion

---
+++ script/generateJsonFile.js
@@ -0,0 +1,47 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+const parse = require("csv-parse");
+
+const trimCords = (cord) => {
+  let split = cord.split(".");
+  if (split[1].length > 3) {
+    let substring = split[1];
+    split[1] = Number("." + substring.slice(0, 3));
+  }
+  return split[0] + split[1];
+};
+
+const processFile = async () => {
+  let records = [];
+  const parser = fs.createReadStream(`./uszips.csv`).pipe(
+    parse({
+
+      // CSV options if any
+    })
+  );
+  for await (const record of parser) {
+    if (record[0] !== "zip") {
+      //records.push(record);
+      const zip = Number(record[0])
+      const lat = trimCords(record[1]);
+      const lng = trimCords(record[2]);
+      const city = record[3];
+      const state_name = record[5]
+      const county_name = record[11]
+      const timezone = record[17]
+      const rowLoc = { zip: zip, lat: lat, lng: lng, city: city, state_name: state_name, county_name: county_name, timezone: timezone };
+      records.push(rowLoc)
+    }
+  }
+  return records;
+};
+
+(async () => {
+  const records = await processFile();
+  console.log(records);
+  fs.writeFile ("input.json", JSON.stringify(records), function(err) {
+    if (err) throw err;
+    console.log('complete');
+    }
+);
+})();
